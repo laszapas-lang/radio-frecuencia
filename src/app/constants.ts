c300686723365315e44c55ea1fc8bcdc1ff0de6a
laszapas-lang <laszapas@gmail.com>
2026-04-27 14:57:04 +0200

    Create constants.ts

---
+++ src/app/constants.ts
@@ -0,0 +1,2 @@
+export const STREAM_URL = "https://radioweb-u71993.vm.elestio.app/listen/frecuencia/live";
+export const STATION_API = "https://radioweb-u71993.vm.elestio.app/api/nowplaying/frecuencia";
